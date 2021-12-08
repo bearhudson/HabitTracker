70aa040c077ac40d2a230ddce3eb1575cf1da358
Brian Hudson <brian.hudson@flexential.com>
2021-12-08 08:32:38 -0500

    fixed post syntax and adding user input

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ username = os.environ.get('USERNAME')
 endpoint = "https://pixe.la/v1/users/"
 
 today = time.strftime("%Y%m%d")
-print(today)
 
 headers = {
     "X-USER-TOKEN": token,
@@ -15,11 +14,17 @@ headers = {
 
 graph_id = "a1"
 
+pixel_endpoint = f"{endpoint}{username}/graphs/{graph_id}"
+
+hours = input("How many hours today? ")
+
 payload = {
     "date": today,
-    "quantity": "1",
+    "quantity": hours,
 }
 
-pixel_endpoint = f"{endpoint}/{username}/graphs/{graph_id}"
-req = requests.post(url=pixel_endpoint, json=payload, headers=headers)
-print(req.text)
+try:
+    req = requests.post(url=pixel_endpoint, json=payload, headers=headers)
+    print(req.raise_for_status())
+except requests.exceptions.RequestException as exception:
+    raise SystemExit(exception)
